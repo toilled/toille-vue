2ef4f071e91756573327511ad7e08f631b56375d
Elliot Dickerson <elliot@toille.uk>
2025-12-27 18:00:28 +0000

    Add database details to wrangler.toml

--- .wrangler/tmp/dev-SxHxUr/functionsWorker-0.8142587415590123.js
+++ .wrangler/tmp/dev-SxHxUr/functionsWorker-0.8142587415590123.js
@@ -715,7 +715,7 @@ var jsonError2 = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx
 }, "jsonError");
 var middleware_miniflare3_json_error_default2 = jsonError2;
 
-// .wrangler/tmp/bundle-kLlgjl/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-i83qfE/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__2 = [
   middleware_ensure_req_body_drained_default2,
   middleware_miniflare3_json_error_default2
@@ -747,7 +747,7 @@ function __facade_invoke__2(request, env, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__2, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-kLlgjl/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-i83qfE/middleware-loader.entry.ts
 var __Facade_ScheduledController__2 = class ___Facade_ScheduledController__2 {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;

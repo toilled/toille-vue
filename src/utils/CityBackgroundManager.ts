e617005876b17e0464f9486530d3b3aeb9fbfc0b
Elliot Dickerson <elliot@toille.uk>
2026-05-10 17:24:06 +0100

    Remember background choice across visits

--- src/utils/CityBackgroundManager.ts
+++ src/utils/CityBackgroundManager.ts
@@ -1,7 +1,21 @@
-import { ref } from "vue";
+import { ref, watch } from "vue";
+
+const STORAGE_KEY = "city-background-enabled";
 
 class CityBackgroundManager {
   isEnabled = ref(true);
+
+  constructor() {
+    if (typeof window !== "undefined") {
+      const stored = localStorage.getItem(STORAGE_KEY);
+      if (stored !== null) {
+        this.isEnabled.value = stored === "true";
+      }
+      watch(this.isEnabled, (val) => {
+        localStorage.setItem(STORAGE_KEY, String(val));
+      });
+    }
+  }
 
   toggle() {
     this.isEnabled.value = !this.isEnabled.value;

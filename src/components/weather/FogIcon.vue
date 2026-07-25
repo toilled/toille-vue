3c2d0bbdb04a0c86bef318da68687cb05de6fa12
Elliot Dickerson <elliot@toille.uk>
2026-07-26 00:49:25 +0100

    refactor: split WeatherIcon into smaller components
    Extract SVG icons into separate components (SunIcon, CloudIcon, FogIcon, RainIcon, SnowIcon, ThunderIcon, UnknownIcon) and WeatherModal to reduce template complexity from 494 lines to ~80 lines

---
+++ src/components/weather/FogIcon.vue
@@ -0,0 +1,30 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="icon"
+    :title="title"
+  >
+    <path d="M4 14h24" />
+    <path d="M4 19h24" />
+    <path d="M4 9h24" />
+  </svg>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  title?: string;
+}>();
+</script>
+
+<style scoped>
+.icon {
+  width: 100%;
+  height: 100%;
+}
+</style>
